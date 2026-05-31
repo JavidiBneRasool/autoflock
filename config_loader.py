59e84caecae7a8c4eaf7a81821a3acf2dad37a2f
iBneRasool <ibnerasool@muslim.com>
2026-05-31 10:32:52 +0530

    Pre-sync checkpoint: 2026-05-31 10:32

--- config_loader.py
+++ config_loader.py
@@ -28,6 +28,9 @@ from datetime import datetime
 # Import VaultFlock
 import sys
 sys.path.insert(0, str(Path.home() / 'projects/media/omni'))
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))
 from vault_agent import VaultFlock
 
 
